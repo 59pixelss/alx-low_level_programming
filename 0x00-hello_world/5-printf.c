e3d48302284e0fbf127bcca348ff4203f181de77
59pixelss <59pixelss@gmail.com>
2023-02-17 00:53:25 -0800

    tryyy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-ssis
+#include <stdio.h>
+/**
+ * main - A program that print a linr using the printf function
+ * Return: 0(Sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+returin (0);
+}
+
