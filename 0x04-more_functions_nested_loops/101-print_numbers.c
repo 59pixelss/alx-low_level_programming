d4e16bce758b9602ae70c1b129b2f4050bc1e831
59pixelss <59pixelss@gmail.com>
2023-02-24 12:40:58 -0800

    aaaaa

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
+ *Owned by Bwave ICT/ Bright Daniel
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+
